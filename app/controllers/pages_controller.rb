93fcaeba59a5ba54bd95e2583df47bd30b533c3f
ryan boyd <ryantylerboyd@outlook.com>
2022-11-22 15:17:20 +1100

    created the product search action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
   def home
-    @products = Products.all
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,12 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def search
+    if params[:product][:category].present?
+      @products = Product.where(category: params[:product][:category])
+    else
+      @products = Product.all
+    end
+  end
 end
