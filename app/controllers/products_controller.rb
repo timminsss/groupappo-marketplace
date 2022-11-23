d41fc38d0c40c27d230ac06dcf1343598dca9499
Jan Schuetz <janalexschuetz@gmail.com>
2022-11-23 12:53:02 +1100

    Implemented various layout changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index search]
   before_action :set_product, only: %i[edit update show_owner destroy]
 
   def index
